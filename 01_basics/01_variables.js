b9ad928d778047f5a54ed0746ebd5e6c3dff2761
ayushiVerma <ayushiver@gmail.com>
2023-12-15 08:17:06 +0000

    Added dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 122553;
+let accountEmail = 'ayushiver@gmail.com';
+var accountPassword = "rtyuigf";
+accountCity = "Bangalore";
+
+console.log(accountEmail);
+
+// prefer to use let and const only. Avoid using var because of issue in block & functional scope
+
+console.table([accountEmail, accountId, accountPassword, accountPassword]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+// number => 2 to the power 53
+// bigint
+//  string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log( typeof null); //object
+console.log(typeof undefined); //undefined 
